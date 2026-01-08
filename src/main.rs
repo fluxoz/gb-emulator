e9a6e975c663193c344743d32eefbf562ac3b290
fluxoz <18295572+fluxoz@users.noreply.github.com>
2026-01-07 21:24:40 -0800

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+mod clock;
+mod cpu;
+mod flags;
+mod gpu;
+mod opcodes;
+
+use flags::FlagsRegister;
+use cpu::CPU;
+use opcodes::load_opcodes;
+
+
+/* 
+Register codes
+0, 000 -> B
+1, 001 -> C
+2, 010 -> D
+3, 011 -> E
+4, 100 -> H
+5, 101 -> L
+6, 110 -> HL
+7, 111 -> A
+*/
+
+static mut RAM: [u8; 8192] = [0; 8192];
+static mut VRAM: [u8; 8192] = [0; 8192];
+
 fn main() {
-    println!("Hello, world!");
+    println!("main!");
+    let (un, cb) = load_opcodes().unwrap();
+    println!("{:?}", un.first());
+    println!("{:?}", cb.first());
+
 }
